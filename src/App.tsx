c40bbeb15fa8d91a0ca98b2da0f7a171573059df
Andrey Kats <andreykats@gmail.com>
2023-01-18 14:52:58 -0500

    adjusted constants

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { Route, Routes } from "react-router-dom"
 import './App.css';
 import { OpenAPI } from './client';
 
-import Layout from './components/Layout';
+import Layout from './Layout';
 import Nopage from './components/Nopage';
 import GridView from './components/GridView';
 import HistoryView from "./components/HistoryView";
 import ImageView from './components/ImageView';
 import CanvasView from './components/CanvasView';
 
-OpenAPI.BASE = 'http://localhost:8000'
+OpenAPI.BASE = 'http://localhost:8080'
 
 function App() {
   return (
