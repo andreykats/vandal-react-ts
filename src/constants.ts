d32ca4194aa75fdb17b54ad1f348b4f63e8f7754
Andrey Kats <andreykats@gmail.com>
2023-01-20 16:15:22 -0500

    fixed path in constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const API_IP_PORT = "localhost:8080"
-export const API_URL = "http://" + API_IP_PORT + "/";
-export const API_WS = "ws://" + API_IP_PORT + "/live/";
+export const API_URL = "http://" + API_IP_PORT
+export const API_WS = "ws://" + API_IP_PORT + "/live/"
 export const API_WS_BROADCAST = API_WS
-export const API_IMAGES = API_URL + "images/"
-export const API_SUBMIT = API_URL + "art/submit/"
+export const API_IMAGES = API_URL + "/images/"
+export const API_SUBMIT = API_URL + "/art/submit/"
